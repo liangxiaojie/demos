47d1f052fa3646c7f4e7f79fcca61d337b2930ab
liangxiaojie <liangxiaojie@genedock.com>
2016-03-21 23:30:44 +0800

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+var config = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    path.resolve(__dirname, 'app/main.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  },
+  plugins: [new htmlWebpackPlugin()]
+};
+
+module.exports = config;
