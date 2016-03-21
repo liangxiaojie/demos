307ec351e5f90594772ea4035b19ffbb4aa99364
liangxiaojie <liangxiaojie@genedock.com>
2016-03-21 23:57:25 +0800

    update query

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ var config = {
       exclude: /(node_modules|bower_components)/,
       loader: 'babel',
       query: {
-        presets: ['react', 'es2015']
+        presets: ['react', 'es2015'],
+        cacheDirectory: true
       }
     }, {
       test: /\.css$/,
