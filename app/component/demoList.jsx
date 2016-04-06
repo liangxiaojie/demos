e3745fb3b119efc400b1a514b46e51a1818b3137
liangxiaojie <liangxiaojie@genedock.com>
2016-04-06 16:28:26 +0800

    click bind

--- app/component/demoList.jsx
+++ app/component/demoList.jsx
@@ -3,29 +3,17 @@ import Hello from './helloWorld.jsx';
 import Hello1 from './helloWorld1.jsx';
 
 export default class DemoList extends React.Component {
+  tabChange (tabName) {
+    // this.setState({currentTab: tabName});
+    console.log(tabName);
+  };
   render () {
-    this.state = this.state || {};
-    var compNames = this.state.compNames || [];
-    var compMap = {
-      hello: Hello,
-      hello1: Hello1
-    }
     return (
-      <div>
-        <ul>
-          <li onClick={() => {
-            this.setState({compNames: 'hello'})
-          }}>helloWorld</li>
-          <li onClick={() => {
-            this.setState({compNames: 'hello1'})
-          }}>helloWorld1</li>
-        </ul>
-        <div>
-          {compNames.map((name, idx) => {
-            return compMap[name];
-          }).toArray()}
-        </div>
-      </div>
+      <ul>
+        <li onClick={this.tabChange.bind(this, 'helloWorld')}>helloWorld</li>
+        <li onClick={this.tabChange.bind(this, 'helloWorld1')}>helloWorld1</li>
+      </ul>
+
     );
-  }
+  };
 };
