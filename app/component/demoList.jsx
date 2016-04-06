e18c412afc9586d5026524c9dbd049960b1390bb
liangxiaojie <liangxiaojie@genedock.com>
2016-04-06 17:20:50 +0800

    content

--- app/component/demoList.jsx
+++ app/component/demoList.jsx
@@ -3,17 +3,24 @@ import Hello from './helloWorld.jsx';
 import Hello1 from './helloWorld1.jsx';
 
 export default class DemoList extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      currentTab: 'helloWorld'
+    };
+  }
   tabChange (tabName) {
-    // this.setState({currentTab: tabName});
-    console.log(tabName);
+    this.setState({currentTab: tabName});
   };
   render () {
     return (
-      <ul>
-        <li onClick={this.tabChange.bind(this, 'helloWorld')}>helloWorld</li>
-        <li onClick={this.tabChange.bind(this, 'helloWorld1')}>helloWorld1</li>
-      </ul>
-
+      <div>
+        <ul>
+          <li onClick={this.tabChange.bind(this, 'helloWorld')}>helloWorld</li>
+          <li onClick={this.tabChange.bind(this, 'helloWorld1')}>helloWorld1</li>
+        </ul>
+        <div>{this.state.currentTab}</div>
+      </div>
     );
   };
 };
